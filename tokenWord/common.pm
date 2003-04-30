145562c96722f276700ee3c3863d6ed148936431
Jason Rohrer <jasonrohrer@fastmail.fm>
2003-04-30 17:09:59 +0000

    Added skeleton for using db database instead of filesystem.

--- tokenWord/common.pm
+++ tokenWord/common.pm
@@ -16,6 +16,9 @@ package tokenWord::common;
 # Moved data directory to a safer location (so tw directory does not
 # have to be world-writeable).
 #
+# 2003-April-30   Jason Rohrer
+# Added skeleton for using db database instead of filesystem.
+#
 
 
 # define our exported variables and subroutines
@@ -28,9 +31,17 @@ sub BEGIN {
                  $dataDirectory
                  $htmlDirectory
                  printFile
+                 bypass_printFile
                  readFileValue
+                 bypass_readFileValue
+                 doesFileExist
+                 bypass_doesFileExist
                  writeFile
+                 bypass_writeFile
                  addToFile
+                 bypass_addToFile
+                 makeDirectory
+                 bypass_makeDirectory
                  trimWhitespace
                  extractRegionComponents );
 }
@@ -41,6 +52,11 @@ $dataDirectoryName = "tokenWordData";
 $dataDirectory = "../../cgi-data/tokenWordData";
 $htmlDirectory = "htmlTemplates";
 
+$dbFile = "../../cgi-data/tokenWordData.db"
+
+# 1 to use db file
+# 0 to use filesytem
+$useDB = 1
 
 
 ##
@@ -52,6 +68,27 @@ $htmlDirectory = "htmlTemplates";
 # printFile( "myFile.txt" );
 ##
 sub printFile {
+    my $fileName = $_[0];
+
+    if( $useDB ) {
+    }
+    else {    
+        bypass_printFile( $fileName );
+    }
+}
+
+
+
+##
+# Prints the contents of a file (from filesystem, ignoring any db)
+# to standard out.
+#
+# @param0 the name of the file.
+#
+# Example:
+# bypass_printFile( "myFile.txt" );
+##
+sub bypass_printFile {
     my $fileName = $_[0];
     open( FILE, "$fileName" ) or die;
     flock( FILE, 1 ) or die;
@@ -77,6 +114,28 @@ sub printFile {
 ##
 sub readFileValue {
     my $fileName = $_[0];
+    
+    if( $useDB ) {
+    }
+    else {
+        bypass_readFileValue( $fileName );
+    }
+}
+
+
+
+##
+# Reads file as a string, accessing the filesystem directly (ignoring any db).
+#
+# @param0 the name of the file.
+#
+# @return the file contents as a string.
+#
+# Example:
+# my $value = bypass_readFileValue( "myFile.txt" );
+##
+sub bypass_readFileValue {
+    my $fileName = $_[0];
     open( FILE, "$fileName" ) or die;
     flock( FILE, 1 ) or die;
 
@@ -92,6 +151,50 @@ sub readFileValue {
 
 
 ##
+# Checks if a file exists.
+#
+# @param0 the name of the file.
+#
+# @return 1 if it exists, and 0 otherwise.
+#
+# Example:
+# $exists = doesFileExist( "myFile.txt" );
+##
+sub doesFileExist {
+    my $fileName = $_[0];
+        
+    if( $useDB ) {
+    }
+    else {
+        return bypass_doesFileExist( $fileName ); 
+    }
+}
+
+
+
+##
+# Checks if a file exists in the filesystem, ignoring any db.
+#
+# @param0 the name of the file.
+#
+# @return 1 if it exists, and 0 otherwise.
+#
+# Example:
+# $exists = bypass_doesFileExist( "myFile.txt" );
+##
+sub bypass_doesFileExist {
+    my $fileName = $_[0];
+    if( -e $fileName ) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+
+
+##
 # Writes a string to a file.
 #
 # @param0 the name of the file.
@@ -101,15 +204,36 @@ sub readFileValue {
 # writeFile( "myFile.txt", "the new contents of this file" );
 ##
 sub writeFile {
-
     my $fileName = $_[0];
     my $stringToPrint = $_[1];
+    
+    if( $useDB ) {
+    }
+    else {
+        bypass_writeFile( $fileName, $stringToPrint );
+    }
+}
 
+
+
+##
+# Writes a string to a file in the filesystem, bypassing any db.
+#
+# @param0 the name of the file.
+# @param1 the string to print.
+#
+# Example:
+# bypass_writeFile( "myFile.txt", "the new contents of this file" );
+##
+sub bypass_writeFile {
+    my $fileName = $_[0];
+    my $stringToPrint = $_[1];
+    
     open( FILE, ">$fileName" ) or die;
     flock( FILE, 2 ) or die;
 
     print FILE $stringToPrint;
-
+        
     close FILE;
 }
 
@@ -125,16 +249,77 @@ sub writeFile {
 # addToFile( "myFile.txt", "the new contents of this file" );
 ##
 sub addToFile {
-
     my $fileName = $_[0];
     my $stringToPrint = $_[1];
+        
+    if( $useDB ) {
+    }
+    else {
+        bypass_addToFile( $fileName, $stringToPrint );
+    }
+}
 
+
+
+##
+# Appends a string to a file in the filesystem, bypassing any db.
+#
+# @param0 the name of the file.
+# @param1 the string to append.
+#
+# Example:
+# bypass_addToFile( "myFile.txt", "the new contents of this file" );
+##
+sub bypass_addToFile {
+    my $fileName = $_[0];
+    my $stringToPrint = $_[1];
+        
     open( FILE, ">>$fileName" ) or die;
     flock( FILE, 2 ) or die;
-
+        
     print FILE $stringToPrint;
-
+        
     close FILE;
+}
+
+
+
+##
+# Makes a directory file.
+#
+# @param0 the name of the directory.
+# @param1 the octal permission mask.
+#
+# Example:
+# makeDirectory( "myDir", oct( "0777" ) );
+##
+sub makeDirectory {
+    my $fileName = $_[0];
+    my $permissionMask = $_[1];
+    
+    if( $useDB ) {
+    }
+    else {
+        bypass_makeDirectory( $fileName, $permissionMask );
+    }
+}
+
+
+
+##
+# Makes a directory file in the filesystem, bypassing any db.
+#
+# @param0 the name of the directory.
+# @param1 the octal permission mask.
+#
+# Example:
+# bypass_makeDirectory( "myDir", oct( "0777" ) );
+##
+sub bypass_makeDirectory {
+    my $fileName = $_[0];
+    my $permissionMask = $_[1];
+    
+    mkdir( $fileName, $permissionMask );
 }
 
 
